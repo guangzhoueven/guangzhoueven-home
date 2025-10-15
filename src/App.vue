8249d62a54788edfd0c97aba0bbc012934c5bc92
guangzhoueven <10062132@qq.com>
2025-10-15 17:27:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -111,8 +111,12 @@ onMounted(() => {
   const styleContent = "color: rgb(30,152,255);";
   const title1 = "主页";
   const title2 = `
-guangzhou_even;
-`
+ _____ __  __  _______     ____     __
+|_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
+  | | | \\  / | (___  \\ \\_/ /  \\ \\_/ /
+  | | | |\\/| |\\___ \\  \\   /    \\   /
+ _| |_| |  | |____) |  | |      | |
+|_____|_|  |_|_____/   |_|      |_|`;
   const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
 });
