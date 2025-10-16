be13a05cc204e6ce840d195594ea1bbf4690a369
guangzhoueven <10062132@qq.com>
2025-10-16 18:04:15 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,6 +44,7 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Box, Toolbox, Gamepad, Bomb, Markdown } from "@vicons/fa"; // 注意使用正确的类别
+import { TetrisApp48Filled } from "@vicons/fluent";
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -74,7 +75,8 @@ const siteIcon = {
   Toolbox,
   Gamepad,
   Bomb,
-  Markdown
+  Markdown,
+  TetrisApp48Filled
 };
 
 // 链接跳转
