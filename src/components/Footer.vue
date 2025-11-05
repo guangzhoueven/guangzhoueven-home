620184c18832206487814dc6e5732a74a975d984
guangzhoueven <10062132@qq.com>
2025-11-05 22:15:05 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,8 @@
         <!-- 站点备案 -->
         <span>
           &amp;
-          <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
+          <!-- <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank"> -->
+          <a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20256363" target="_blank">
             {{ siteIcp }}
           </a>
         </span>
